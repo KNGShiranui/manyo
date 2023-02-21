74146059a658e374185a47fe72787afda6e9a773
kengo <mathematics9973@gmail.com>
2023-02-21 13:51:20 +0900

    Create a controller that defines the actions on the task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,67 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit update destroy ]
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    if params[:back]
+      @task = Task.new(task_params)
+    else
+      @task = Task.new
+    end
+  end
+
+  def create
+    @task = Task.new(task_params)
+    respond_to do |format|
+      if params[:back]
+        render :new
+      else
+        if @task.save
+          format.html { redirect_to task_url(@task), notice: "タスクが作成されました" }
+          format.json { render :show, status: :created, location: @task }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @task.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to task_url(@task), notice: "タスクが更新されました" }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @task.destroy  
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: "タスクが削除されました" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
