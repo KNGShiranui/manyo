ca9c576277a71462844da8a94fd5baaac56a78d6
kengo <mathematics9973@gmail.com>
2023-02-23 17:19:52 +0900

    setting of due_date

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -58,7 +58,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :due_date)
   end
 
   def set_task
