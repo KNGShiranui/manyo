722ea1b35f38fea0fd93e253d32f70e15a721bf8
kengo <mathematics9973@gmail.com>
2023-02-23 17:02:46 +0900

    setting of due_date

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 80}
   validates :content, presence: true,  length: { maximum: 1000}
+  validates :due_date, presence: true
   default_scope -> { order(created_at: :desc) }
 end
