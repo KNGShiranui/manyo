792a5120956d6211f2b4bdb2ac30dad06ebe526d
kengo <mathematics9973@gmail.com>
2023-02-27 18:12:36 +0900

    fix views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "SW", email: 'SW@example.com', password: '11101252', password_confirmation: '11101252', administrator: true)
+User.create!(name: "SW2", email: 'SW2@example.com', password: '11101252', password_confirmation: '11101252', administrator: true)
